54982a550e0e70d0f78b1d31d7507edae93e52e3
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2024-06-01 22:49:09 -0300

    feat: Vercel CORS

--- src/components/catalog/(common)/Frame.tsx
+++ src/components/catalog/(common)/Frame.tsx
@@ -1,19 +1,17 @@
 import Button from "@/components/(common)/Button";
 import styles from "./Frame.module.css"
 import Image, { StaticImageData } from "next/image";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Frame(props: { title: string, imageSrc: StaticImageData, styleName: string }) {
-  const { push } = useRouter()
-
   return (
-    <div className={`${styles.container} ${styles[props.styleName]}`} onClick={() => push(`http://${process.env.VERCEL_URL}/products/${props.title}`)}>
+    <Link href={`http://${process.env.VERCEL_URL}/products/${props.title}`} className={`${styles.container} ${styles[props.styleName]}`}>
       <div>
         <div className={styles.imageWrap}>
           <Image src={props.imageSrc} alt={props.title} fill/>
         </div>
         <Button text={props.title} styleNames={["catalog", props.styleName]}/>
       </div>
-    </div>   
+    </Link>   
   )
 }
